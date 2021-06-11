9aebbb61172bc3d76d68647e500c2aef82d0bdc9
Rodion <rodion2001vo@gmail.com>
2021-06-11 14:40:06 +0300

    Todolist
    FirstApp

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>Планувальник завдань</h1>
+    </div>
+  );
+};
+
+export default Header;
